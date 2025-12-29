65ffd64930089abbd184fb0bffbe76fcbf33f770
Icradle-Innovations-Ltd <amon.muhwezik@gmail.com>
2025-12-29 12:00:21 +0300

    Bump versionCode to 3 in build.gradle.kts
    Updated the versionCode from 2 to 3 for the Android app, preparing for a new release or update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.flux.recorder"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
