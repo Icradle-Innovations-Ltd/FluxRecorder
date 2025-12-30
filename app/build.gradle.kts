c360c6ad78357ca0d6b03ba3bc24c82cbcf22e5f
Icradle-Innovations-Ltd <amon.muhwezik@gmail.com>
2025-12-30 17:51:27 +0300

    Bump app version to 1.1.0 (versionCode 4)
    Updated versionCode to 4 and versionName to 1.1.0 in build.gradle.kts to prepare for a new release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.flux.recorder"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
